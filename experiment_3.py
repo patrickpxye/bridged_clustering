e19c1d6ff5c77ac2c8ad72962813f0608bee238b
Patrick Peixuan Ye <patrickypx@gmail.com>
2024-06-13 16:36:32 +0800

    experiment report

--- experiment_3.py
+++ experiment_3.py
@@ -12,8 +12,19 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+def decision(matrix):
+    max_indices = np.argmax(matrix, axis=1)
+    if max_indices[0] == max_indices[1]:
+        differences = np.abs(np.diff(matrix, axis=1)).flatten()
+        row_with_smaller_difference = np.argmin(differences)
+        max_indices[row_with_smaller_difference] = 1 - max_indices[row_with_smaller_difference]
+    return max_indices
+
+
+
 df = pd.read_csv('herb_data_filtered.csv')
-feature_list_1 =["Lobe.number","BW","TLIW","LLL","LSR","LLDW","LLIW"]
+#feature_list_1 =["Lobe.number","BW","TLIW","LLL","LSR","LLDW","LLIW"]
+feature_list_1 = ["Lobe.number","PL","TLIW","TLL","TLDW","BLL","LSR","LLIW"]
 feature_list_2 = ["Latitude", "Longitude","LLL.LLLDW","BL.BW"]
 
 data = df[feature_list_1].dropna()
@@ -59,7 +70,9 @@ plt.fill_between(x, density, alpha=0.5)  # Fill under the curve for better visua
 
 score = 0
 k = 10000
+sup = 5
 distances = []
+neighbour_locs = []
 for j in range(k):
     test_data = data.sample(1)
     test_id = test_data.index
@@ -77,6 +90,27 @@ for j in range(k):
     distance_from_group_b = np.linalg.norm(test_data_ground_truth - loc_group_b_centroid)
     actual_closer_group = np.argmin([distance_from_group_a, distance_from_group_b])
 
+    #get 3 points closest to the test point
+    random_indices = np.random.choice(len(data_standardized), sup, replace=False)
+    ten_data = data_standardized[random_indices]
+    ten_loc_data = loc_data_standardized[random_indices]
+    n_distances = np.linalg.norm(ten_data - test_point, axis=1)
+    closest_points = np.argsort(n_distances)[:3]
+    average_location = np.mean(ten_loc_data[closest_points], axis=0).reshape(1, -1)
+    neighbour_locs.append(np.linalg.norm(average_location - test_data_ground_truth))
+
+    decision_matrix = np.zeros((2, 2))
+    for i in random_indices:
+        supervised_pt = df.loc[i].to_frame().T
+        #keep only 16 columns of supervised data
+        gmm_pt = supervised_pt[feature_list_1].dropna()
+        kmeans_pt = supervised_pt[feature_list_2].dropna()
+        pred1 = knn.predict(scaler.transform(transformer.transform(gmm_pt)))
+        pred2 = loc_knn.predict(scaler_2.transform(transformer_2.transform(kmeans_pt)))
+        decision_matrix[pred1[0]][pred2[0]] += 1
+
+    decision_vector = decision(decision_matrix)
+
     decision_vector = np.array([0,1])
     test_result = decision_vector[test_point_cluster]
 
@@ -88,6 +122,16 @@ for j in range(k):
         distances.append(distance_from_group_b)
 
 print("Accuracy: ", score/k)
+
+
+kde = gaussian_kde(neighbour_locs)
+x = np.linspace(min(norms), max(norms), 2000)
+density = kde(x)
+
+plt.plot(x, density, label='KNN')
+plt.fill_between(x, density, alpha=0.5)  # Fill under the curve for better visualization
+
+
 
 kde = gaussian_kde(distances)
 x = np.linspace(min(distances), max(distances), 1000)
@@ -105,7 +149,8 @@ plt.show()
 
 norms_mean = np.mean(norms)
 distances_mean = np.mean(np.array(distances))
-print("Error Reduction: ", (norms_mean - distances_mean) / norms_mean)
+neighbour_mean = np.mean(np.array(neighbour_locs))
+print("Error Reduction: ", (neighbour_mean - distances_mean) / neighbour_mean)
 
 #Accuracy:  0.9354
 #Error Reduction:  0.3076861605157626
