f3450133bb86ef784d86da7fa185af9f97c0a3e7
Patrick Peixuan Ye <patrickypx@gmail.com>
2025-02-02 14:58:21 -0800

    update on linear experiements

--- conference/main.py
+++ conference/main.py
@@ -75,6 +75,7 @@ def baselineLinear(super_df, test_morph_df, test_gene_df):
     def euclidean_distance(row):
         return np.linalg.norm(np.array(row['gene_coordinates']) - np.array(row['lin_gene_predictions']))
     test_joined_df['lin_distance'] = test_joined_df.apply(euclidean_distance, axis=1)
+
     result_lin = list(test_joined_df['lin_distance'])
 
     return result_lin
@@ -117,21 +118,71 @@ def simgleExperiment(super_size, n_families, n_neighbors, test_size=20):
     ## Baseline Models
 
     # instantiate baseline models, train with fully supervised set, and test with test set
-    result_knn = baselineKNN(super_df, n_neighbors, test_morph_df, test_gene_df)
+    # result_knn = baselineKNN(super_df, n_neighbors, test_morph_df, test_gene_df)
     result_lin = baselineLinear(super_df, test_morph_df, test_gene_df)
+
+    return result_bkm, [], result_lin
 
     return result_bkm, result_knn, result_lin
 
 
-result_bkm, result_knn, result_lin = [], [], []
+def addExperiment(super_size, n_families, n_neighbors, test_size=20):
+
+    ### Dataset Preparation
+
+    # read and preprocess data
+    morph_df = preprocessMorphData()
+    gene_df = preprocessGeneData()
+
+    # isolate fully labeled dataset: label a fraction as test set and the rest as fully supervised set
+    common_nos = set(gene_df['TreeNo']).intersection(set(morph_df['TreeNo']))
+    test_nos = np.random.choice(list(common_nos), size=test_size, replace=False)
+    super_nos = np.random.choice(list(common_nos - set(test_nos)), size=super_size, replace=False)
+
+    # create test set, then use the rest of the data for training
+    test_morph_df = morph_df[morph_df['TreeNo'].isin(test_nos)]
+    test_gene_df = gene_df[gene_df['TreeNo'].isin(test_nos)]
+    train_morph_df = morph_df[~morph_df['TreeNo'].isin(test_nos)]
+    train_gene_df = gene_df[~gene_df['TreeNo'].isin(test_nos)]
+
+    ## Bridged Clustering
+
+    # train clustering models; formalize data as coordinates; predict clusters for train set, only useful for the fully supervised portion
+    morph_df, morph_kmeans = fitMorphClusters(train_morph_df, n_families, full_feat=True)
+    gene_df, gene_kmeans = fitGeneClusters(train_gene_df, n_families, full_feat=False)
+
+    # predict cluster assignment for test set, only useful for the bridged clustering method
+    test_morph_df = predictMorphClusters(test_morph_df, morph_kmeans, full_feat=True)
+    test_gene_df = predictGeneClusters(test_gene_df, gene_kmeans, full_feat=False)
+
+    # build cluster associations through supervised set, and run bridged clustering on test set
+    joined_df = morph_df.merge(gene_df, on='TreeNo')
+    super_df = joined_df[joined_df['TreeNo'].isin(super_nos)]
+    # result_bkm = bridgedClustering(super_df, gene_df, test_morph_df, test_gene_df)
+
+    ## Baseline Models
+
+    # instantiate baseline models, train with fully supervised set, and test with test set
+    result_knn = baselineKNN(super_df, n_neighbors, test_morph_df, test_gene_df)
+    # result_lin = baselineLinear(super_df, test_morph_df, test_gene_df)
+
+    return [], result_knn, []
+
+result_bkm, result_lin = [], []
+result_knn_1, result_knn_2, result_knn_3 = [], [], []
 samplesize = [6, 11, 17, 22, 28, 33]
 n_families = 3
 outputfeaturelist = ['PC1', 'PC2', 'PC3']
-for i in range(10):
-    for super_size in [samplesize[2]]:
-        bkm, knn, lin = simgleExperiment(super_size,n_families,1)
+for i in range(1000):
+    for super_size in [samplesize[0]]:
+        bkm, _, lin = simgleExperiment(super_size,n_families,1)
+        _, knn, _ = addExperiment(super_size,n_families,1)
         result_bkm.extend(bkm)
         result_lin.extend(lin)
-        result_knn.extend(knn)
+        result_knn_1.extend(knn)
+        _, knn, _ = addExperiment(super_size,n_families,2)
+        result_knn_2.extend(knn)
+        _, knn, _ = addExperiment(super_size,n_families,3)
+        result_knn_3.extend(knn)
 
-plotDensityGraph(result_bkm, result_knn, result_lin)
+plotDensityGraph(result_bkm, result_knn_1, result_knn_2, result_knn_3, result_lin)
--- conference/utils.py
+++ conference/utils.py
@@ -28,7 +28,7 @@ def decisionVector(sample, dim=3):
         return decision
 
 
-def plotDensityGraph(result_bkm, result_knn, result_lin):
+def plotDensityGraph(result_bkm, result_knn_1, result_knn_2, result_knn_3, result_lin):
 
     kde = gaussian_kde(result_bkm)
     x = np.linspace(min(result_bkm), max(result_bkm), 2000)
@@ -39,27 +39,45 @@ def plotDensityGraph(result_bkm, result_knn, result_lin):
     print("mean distance for bkm is ", mean_bkm)
     plt.axvline(mean_bkm, color='blue', linestyle='dashed', linewidth=1)
 
-    kde = gaussian_kde(result_lin)
-    x = np.linspace(min(result_lin), max(result_lin), 2000)
-    density = kde(x)
-    plt.plot(x, density, label='Linear Regression')
-    plt.fill_between(x, density, alpha=0.5)
-    mean_lin = np.mean(result_lin)
-    print("mean distance for linear regression is ", mean_lin)
-    plt.axvline(mean_lin, color='green', linestyle='dashed', linewidth=1)
+    # kde = gaussian_kde(result_lin)
+    # x = np.linspace(min(result_lin), max(result_lin), 2000)
+    # density = kde(x)
+    # plt.plot(x, density, label='Linear Regression')
+    # plt.fill_between(x, density, alpha=0.5)
+    # mean_lin = np.mean(result_lin)
+    # print("mean distance for linear regression is ", mean_lin)
+    # plt.axvline(mean_lin, color='green', linestyle='dashed', linewidth=1)
 
-    kde = gaussian_kde(result_knn)
-    x = np.linspace(min(result_knn), max(result_knn), 2000)
+    kde = gaussian_kde(result_knn_1)
+    x = np.linspace(min(result_knn_1), max(result_knn_1), 2000)
     density = kde(x)
-    plt.plot(x, density, label='KNN')
+    plt.plot(x, density, label='KNN(k=1)')
     plt.fill_between(x, density, alpha=0.5)
-    mean_knn = np.mean(result_knn)
-    print("mean distance for knn is ", mean_knn)
+    mean_knn = np.mean(result_knn_1)
+    print("mean distance for KNN(k=1) is ", mean_knn)
+    plt.axvline(mean_knn, color='red', linestyle='dashed', linewidth=1)
+
+    kde = gaussian_kde(result_knn_2)
+    x = np.linspace(min(result_knn_2), max(result_knn_2), 2000)
+    density = kde(x)
+    plt.plot(x, density, label='KNN(k=2)')
+    plt.fill_between(x, density, alpha=0.5)
+    mean_knn = np.mean(result_knn_2)
+    print("mean distance for KNN(k=2) is ", mean_knn)
+    plt.axvline(mean_knn, color='pink', linestyle='dashed', linewidth=1)
+
+    kde = gaussian_kde(result_knn_3)
+    x = np.linspace(min(result_knn_3), max(result_knn_3), 2000)
+    density = kde(x)
+    plt.plot(x, density, label='KNN(k=3)')
+    plt.fill_between(x, density, alpha=0.5)
+    mean_knn = np.mean(result_knn_3)
+    print("mean distance for KNN(k=3) is ", mean_knn)
     plt.axvline(mean_knn, color='red', linestyle='dashed', linewidth=1)
 
 
     plt.xlabel('Euclidean Distance', fontsize=16)
     plt.ylabel('Frequency', fontsize=16)
     plt.legend(fontsize=16)
-    plt.savefig('plots/fig.pdf')
+    plt.savefig('plots/5.pdf')
     plt.show()
